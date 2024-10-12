de8b30e55bec3133935b497dddf88b1401b91b75
Murali Varath <muralitharan.balanzer@gmail.com>
2024-10-11 22:51:49 -0400

    save changes

--- typescript-gulp-build/compile_process/tasks/bundle.js
+++ typescript-gulp-build/compile_process/tasks/bundle.js
@@ -4,15 +4,21 @@ const ts = require("gulp-typescript");
 const tsProject = ts.createProject("tsconfig.json");
 
 const browserify = require("browserify");
+const minify = require("gulp-minify");
 const source = require("vinyl-source-stream");
 const tsify = require("tsify");
-
+const cleanCSS = require("gulp-clean-css");
 const terser = require("gulp-terser");
 const sourcemaps = require("gulp-sourcemaps");
 const buffer = require("vinyl-buffer");
 
+/**
 gulp.task("compile", function () {
   return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest(paths.output));
+}); */
+
+gulp.task("minify-css", function () {
+  return gulp.src(paths.style).pipe(cleanCSS()).pipe(gulp.dest(paths.release));
 });
 
 gulp.task("browserify", function () {
@@ -27,10 +33,11 @@ gulp.task("browserify", function () {
     .bundle()
     .pipe(source("bundle.js"))
     .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.init({ loadMaps: false }))
     .pipe(terser())
     .pipe(sourcemaps.write("./"))
+    .pipe(minify())
     .pipe(gulp.dest(paths.release));
 });
 
-gulp.task("bundle", gulp.series("browserify"), function () {});
+gulp.task("bundle", gulp.series("minify-css", "browserify"), function () {});
