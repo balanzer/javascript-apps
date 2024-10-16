43b1e230809f5fc82e7cb5a558418417e0442bea
Murali Varath <muralitharan.balanzer@gmail.com>
2024-10-15 21:30:03 -0400

    updates

--- plopjs-generate-files/plopfile.js
+++ plopjs-generate-files/plopfile.js
@@ -1,3 +1,9 @@
+/**
+ * Validation functions
+ * @param {} year
+ * @returns
+ */
+
 const isValidYear = year => {
   return value => {
     if (!!value && value.trim().length == 4) {
@@ -7,35 +13,60 @@ const isValidYear = year => {
   };
 };
 
+/**
+ * Plop generator
+ * @param {} plop
+ */
 export default function (plop) {
+  //set helpers
+  plop.setHelper("upperCase", txt => txt.toUpperCase());
+
+  plop.setHelper("leadingZeros", testNumber => {
+    if (testNumber < 10000) {
+      return `0${testNumber}`;
+    }
+    return testNumber;
+  });
+
   // controller generator
   plop.setGenerator("module", {
     description: "application controller logic",
     prompts: [
       {
-        type: "input",
-        name: "year",
-        message: "What Year?",
-        validate: isValidYear("year"),
-      },
-      {
         type: "list",
-        name: "dataConfig",
-        message: "Tell me about the data, what do you need?",
+        name: "year",
+        message: "Select test year : ",
         default: "none",
         choices: [
-          { name: "A", value: "A" },
-          { name: "B", value: "B" },
-          { name: "C", value: "C" },
-          { name: "D", value: "D" },
+          { name: "2024", value: "2024" },
+          { name: "2025", value: "2025" },
+          { name: "2026", value: "2026" },
         ],
       },
+      {
+        type: "input",
+        name: "testNumber",
+        message: "Enter test number between 01 to 99 : ",
+        validate: function (value) {
+          var digitsOnly = /\d+/;
+          if (digitsOnly.test(value)) {
+            const numval = parseInt(value);
+            if (numval > 0 && numval <= 99) {
+              return true;
+            }
+          }
+          return "Invalid test number ! Must be between 01 to 99";
+        },
+      },
     ],
+
     actions: [
       {
         type: "add",
         path: "src-genrated/{{year}}.js",
-        templateFile: "src-templates/controller.hbs",
+        templateFile: "src-templates/controller.txt",
+        abortOnFail: true,
+        skipIfExists: true,
       },
     ],
   });
