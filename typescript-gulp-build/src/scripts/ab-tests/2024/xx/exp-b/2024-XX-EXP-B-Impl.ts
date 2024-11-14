1e6f854aacfd158f31a6ef9ef628bf5f77d3098c
Murali Varath <muralitharan.balanzer@gmail.com>
2024-11-14 14:28:24 -0500

    save changwes

--- typescript-gulp-build/src/scripts/ab-tests/2024/xx/exp-b/2024-XX-EXP-B-Impl.ts
+++ typescript-gulp-build/src/scripts/ab-tests/2024/xx/exp-b/2024-XX-EXP-B-Impl.ts
@@ -2,7 +2,7 @@ import { CommonFeatures } from "../common/common-features";
 
 export class Impl_2024_XX_EXP_B extends CommonFeatures {
   constructor() {
-    super("hello-my-id");
+    super("2024-XX-EXP-B");
   }
 
   getPerimeter(): void {
