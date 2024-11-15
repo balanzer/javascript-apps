a67b5f468311f074a47ad6237ce4ea6833b62621
Murali Varath <muralitharan.balanzer@gmail.com>
2024-11-15 09:45:41 -0500

    added analytics utils

--- typescript-gulp-build/src/scripts/common/utils/analytics/analytics-utils.ts
+++ typescript-gulp-build/src/scripts/common/utils/analytics/analytics-utils.ts
@@ -2,6 +2,7 @@ import { Logger } from "../../logger/Logger";
 
 export class AnalyticsUtils {
   private logger: Logger;
+  private name: string;
 
   private LOADED: string = "|loaded";
   private CHANGES_APPLIED: string = "|changes-applied";
@@ -10,19 +11,34 @@ export class AnalyticsUtils {
 
   constructor(name: string) {
     const logPrefix = `${name}-analytics :`;
+    this.name = name;
     this.logger = new Logger(logPrefix);
   }
 
   public changesApplied(): void {
-    //TODO-Pending
+    const message = `${this.name}${this.CHANGES_APPLIED}`;
+    this.recordMessage(message);
   }
   public loaded(): void {
-    //TODO-Pending
+    const message = `${this.name}${this.LOADED}`;
+    this.recordMessage(message);
   }
   public pollFailure(): void {
-    //TODO-Pending
+    const message = `${this.name}${this.POLL_FAILED}`;
+    this.recordMessage(message);
   }
-  public error(): void {
-    //TODO-Pending
+  public error(errorMessage: string): void {
+    const message = `${this.name}${this.ERROR}${errorMessage}`;
+    this.recordMessage(message);
+  }
+
+  private recordMessage(message: string): void {
+    this.logger.info("message : ", message);
+
+    window.dispatchEvent(
+      new CustomEvent("martech-a4t-custom-message", {
+        detail: message,
+      })
+    );
   }
 }
